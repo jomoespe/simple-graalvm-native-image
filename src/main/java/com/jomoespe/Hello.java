38ec1a8c951f7bd9213470c751425513c50c0b96
José Moreno <jomoespe@gmail.com>
2020-02-28 14:51:24 +0100

    Add dependency to Apache CLI

--- src/main/java/com/jomoespe/Hello.java
+++ src/main/java/com/jomoespe/Hello.java
@@ -1,7 +1,25 @@
 package com.jomoespe;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class Hello {
-    public static void main(String...args) {
-        System.out.printf("Hello %s!\n", args.length>0 ? args[0] : "world");
+    public static void main(String... args) throws ParseException {
+        Options options = new Options();
+
+        Option nameOpt = new Option("n", "name", true, "input file path");
+        nameOpt.setRequired(false);
+        options.addOption(nameOpt);
+
+        CommandLineParser parser = new DefaultParser();
+        //HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = parser.parse(options, args);
+
+        String name = cmd.getOptionValue("name", "world");
+        System.out.printf("Hello %s!!\n", name);
     }
 }
