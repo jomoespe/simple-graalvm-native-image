b1b2e661c06dffdef63c12b7e206cd80894b9011
José Moreno <jomoespe@gmail.com>
2020-02-28 23:30:21 +0100

    Refactor Hello.java

--- src/main/java/com/jomoespe/Hello.java
+++ src/main/java/com/jomoespe/Hello.java
@@ -1,5 +1,6 @@
 package com.jomoespe;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import org.apache.commons.cli.CommandLine;
@@ -9,18 +10,30 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public final class Hello {
-    public static void main(final String... args) throws ParseException {
-        CommandLine cmd = commandLine(Hello::options, () -> args);
-        System.out.printf("Hello %s!\n", cmd.getOptionValue("name", "world"));
+    public static void main(final String... args) {
+        final Optional<CommandLine> cmd = commandLine(Hello::options, () -> args);
+        System.out.printf("Hello %s!\n", name(cmd));
     }
 
-    private static CommandLine commandLine(final Supplier<Options> options, final Supplier<String[]> args) throws ParseException {
-        CommandLineParser parser = new DefaultParser();
-        return parser.parse(options.get(), args.get());
+    private static String name(final Optional<CommandLine> cmd) {
+        final String defaultName = "world";
+        return cmd.isPresent() ? cmd.get().getOptionValue("name", defaultName) : defaultName;
+    }
+
+    private static Optional<CommandLine> commandLine(final Supplier<Options> options, final Supplier<String[]> args) {
+        try {
+            CommandLineParser parser = new DefaultParser();
+            return Optional.of(parser.parse(options.get(), args.get()));
+        } catch (ParseException e) {
+            System.err.printf("Error parsing command line arguments: %s\n", e.getMessage());
+            e.printStackTrace(System.err);
+            return Optional.empty();
+        }
     }
 
     private static Options options() {
-        return new Options()
-            .addOption("n", "name", true, "the name to be salutated");
+        final Options options = new Options()
+            .addOption("n", "name", true, "the name to say hello");
+        return options;
     }
 }
